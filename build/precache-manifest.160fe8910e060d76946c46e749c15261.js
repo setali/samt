5004924d1c54bc09bb8b94cb6453d5f3eb024baa
salimousavi <set.ali.mousavi>
2018-12-02 16:27:21 +0330

    add build

---
+++ build/precache-manifest.160fe8910e060d76946c46e749c15261.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e95ead2baa1780f56050",
+    "url": "/static/js/main.e95ead2b.chunk.js"
+  },
+  {
+    "revision": "e043017739fdeb518117",
+    "url": "/static/js/1.e0430177.chunk.js"
+  },
+  {
+    "revision": "e043017739fdeb518117",
+    "url": "/static/css/1.fd9802de.chunk.css"
+  },
+  {
+    "revision": "c4606524852f182c42c9658f0b93ae84",
+    "url": "/index.html"
+  }
+];
--- src/tools/Utils.js
+++ src/tools/Utils.js
@@ -5,11 +5,25 @@ import {BASE_URL, COOKIE_EXPIRES} from './Constants'
 export function Header(options = {}) {
     let token = Token(), headers = new Headers();
 
-    headers.set('Accept', 'application/json, text/plain, */*');
+    // headers.set('Accept', 'application/json');
+    headers.set('Accept', 'application/x-www-form-urlencoded');
+    headers.set('Accept-Encoding', 'gzip, deflate, br');
+    headers.set('Accept-Language', 'en-US,en;q=0.9,fa;q=0.8');
+    headers.set('Authorization', 'Basic c2FtdC1hZG1pbjphYmNAMTIz');
+    headers.set('Connection', 'keep-alive');
+    headers.set('Content-Length', '13');
     headers.set('Content-Type', 'application/json;charset=UTF-8');
-    headers.set('Origin', 'https://app.nwms.ir');
+    headers.set('Cookie', 'cookiesession1=1D1712079EAOJHPZVZMAT4GOWLDUC0EE; msdCookie=592daa39f5e3aa64259bf00323160d13b3a180324aqZH5Sxoh2IpfrjPLQ/eJS79ZUkeXZxA6WuJ7EOg1lGKduNvxvUnxQSaCDiNoJOL/x7aSXXL43JdYv2wa7fxXt7dPXYOe6sMf1wmWgVrPMFixzfY18UBSSic+fPPq/bsHTYdl4U4e7eTYlJC2GuDmbLndWbP/S5WQHA+MPNj0PX61MfL/IWOCXgix2QfK2RfCLKefgO/shmQqC1x9vy7EPJ1dOnpELTxjFxXjDYLTWqnKHq5991w+s+Ys9utcWEotzovGOoIKuJEEY=');
+    headers.set('Host', 'admin-app.nwms.ir');
+    headers.set('Origin', '*');
+    headers.set('Access-Control-Allow-Origin', '*');
+    // headers.set('Origin', 'https://admin-app.nwms.ir');
+    headers.set('Referer', 'https://admin-app.nwms.ir/cardex');
+    headers.set('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36');
 
-    token && headers.set('Cookie', token);
+
+
+    // token && headers.set('Cookie', token);
     options.headers && Object.entries(options.headers).forEach(([k, v]) => headers.set(k, v));
 
     options.headers = headers;
@@ -21,11 +35,11 @@ export function Url(url, data = undefined) {
     return BASE_URL + url + (data ? `?${qs.stringify(data)}` : ``);
 }
 
-export function setToken(token) {
-    const expires = new Date();
-    expires.setDate(expires.getDate() + COOKIE_EXPIRES);
-    cookie.save('token', token, {path: '/', expires});
-}
+// export function setToken(token) {
+//     const expires = new Date();
+//     expires.setDate(expires.getDate() + COOKIE_EXPIRES);
+//     cookie.save('token', token, {path: '/', expires});
+// }
 
 export function Token() {
     let token = cookie.load('Cookie');
